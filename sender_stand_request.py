76760da92ba503ee2b852f678cba7753fec98636
“Moni2mil” <solismendez.monicav@gmail.com>
2025-04-28 23:10:55 -0600

    Correcciones con maestra Hilda XD

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -7,6 +7,13 @@ def post_new_user(body):
                          json=body,  # inserta el cuerpo de solicitud
                          headers=data.headers)  # inserta los encabezados
 
+#def get_users_table():
+ #   return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH,  # inserta la dirección URL completa
+#                         headers=data.headers)  # inserta los encabezados
+
 response = post_new_user(data.user_body)
+#response2 = get_users_table()
 print(response.status_code)
 print(response.json())
+#print(response2.status_code)
+#print(response2.json())
